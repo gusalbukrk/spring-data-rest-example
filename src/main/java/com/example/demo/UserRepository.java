7ffd5447e5fb2cfab54032cdf395f37603e88a05
gusalbukrk <gusalbukrk@gmail.com>
2023-11-06 10:06:23 -0300

    add new enpoint

--- src/main/java/com/example/demo/UserRepository.java
+++ src/main/java/com/example/demo/UserRepository.java
@@ -1,5 +1,14 @@
 package com.example.demo;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-public interface UserRepository extends JpaRepository<User, Long> {}
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+  // http://localhost:8081/api/users/search/byName?name=gustavo
+  @RestResource(path = "byName")
+  List<User> findAllByName(String name);
+
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,1 @@
-INSERT INTO user (name, email) VALUES ('Luiz', 'luiz@gmail.com'), ('Gustavo', 'gustavo@gmail.com');
+INSERT INTO user (name, email) VALUES ('Luiz', 'luiz@gmail.com'), ('Gustavo', 'gustavo@gmail.com'), ('Gustavo', 'gus@gmail.com');
